4ee0b4877fb1e0fd7d8d5259b676332e02abbf28
ElderSam <eldersamuel98@gmail.com>
2020-08-11 19:09:12 -0300

    adicionando primeira rota com Slim Framework

---
+++ backend/index.php
@@ -0,0 +1,20 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . './vendor/autoload.php'; //para reconhecer classes automaticamente
+
+$app = AppFactory::create(); //cria a aplicação Slim
+
+/*$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});*/
+
+$app->get('/', function () {
+    echo "Hello World!";
+    exit;
+});
+
+$app->run();
